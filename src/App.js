c4a69b3ccf4edc5f5b29538385e83d7a29f1bf64
Hoàng Tiến Đạt <39739436+DatHoang997@users.noreply.github.com>
2020-01-21 17:00:50 +0700

    dathoang

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Web3 from 'web3';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// var connect = () => {
+//   const web3 = new Web3(new Web3.providers.WebsocketProvider("wss://ws.nexty.io"));
+
+//   web3.eth.subscribe("newBlockHeaders", async (error, event) => {
+//   // if (error) return console.error(error);
+//   //   console.log('Successfully subscribed!', event.number);
+//   // })
+//   // .on('data', (event) => {
+//   // console.log('data: ', event);
+//   // });
+//     if (!error) {
+//       var blockNumber = event.number;
+//       console.log(event.number);
+//       return event.number;
+//     }
+//     console.log(error);
+//     return blockNumber;
+//     });
+// }
+// var connect = () => {
+//   var web3 = new Web3(Web3.givenProvider || "ws://178.238.233.73:8546");
+
+//   var blockNumber = web3.eth.getBlockNumber();
+//   return blockNumber;
+// }
+
+class App extends Component {
+  state = {blockNumber: 0}
+  async componentDidMount() {
+  // this.setState({blockNumber: await connect()})
+  this.connect()
+  }
+  connect = () => {
+    const web3 = new Web3(new Web3.providers.WebsocketProvider("wss://ws.nexty.io"));
+    // var a = web3.eth.getBalance("0x045bfe22453a9ca06aff4bdc5d7f5870eba121bd")
+    // console.log(a);
+
+
+    // web3.eth.subscribe("newBlockHeaders", async (error, event) => {
+      // if (!error) {
+      //   var blockNumber = event.number;
+      //   console.log(event);
+      //   this.setState({
+      //     blockNumber: event.number
+      //   })
+      //   return event.number;
+      // }
+      // console.log(error);
+      // return blockNumber;
+      // });
+
+      var subscription = web3.eth.subscribe('logs', {
+        address: '0x57AaeF34Ea26b7dD3E1d9b1EE6C251FD464246B9',
+        topics: ['0xe1fffcc4923d04b559f4d29a8bfc6cda04eb5b0d3c460751c2402c5c5cc9109c']
+    }, function(error, result){
+        if (!error)
+            console.log(result);
+    })
+
+
+    // unsubscribes the subscription
+    subscription.unsubscribe(function(error, success){
+        if(success)
+            console.log('Successfully unsubscribed!');
+    });
+
+    web3._provider.on('end', (eventObj) => {
+      console.log(eventObj);
+    })
+  }
+
+  render() {
+      return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {/* {this.state.blockNumber} */}
+            {}
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
