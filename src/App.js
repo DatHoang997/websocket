869013cfb6c1e032899da4a1ec456be5e41524e0
Hoàng Tiến Đạt <dathoang997@gmail.com>
2020-02-05 16:43:58 +0700

    find transaction of acc

--- src/App.js
+++ src/App.js
@@ -2,75 +2,107 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Web3 from 'web3';
-
-// var connect = () => {
-//   const web3 = new Web3(new Web3.providers.WebsocketProvider("wss://ws.nexty.io"));
-
-//   web3.eth.subscribe("newBlockHeaders", async (error, event) => {
-//   // if (error) return console.error(error);
-//   //   console.log('Successfully subscribed!', event.number);
-//   // })
-//   // .on('data', (event) => {
-//   // console.log('data: ', event);
-//   // });
-//     if (!error) {
-//       var blockNumber = event.number;
-//       console.log(event.number);
-//       return event.number;
-//     }
-//     console.log(error);
-//     return blockNumber;
-//     });
-// }
-// var connect = () => {
-//   var web3 = new Web3(Web3.givenProvider || "ws://178.238.233.73:8546");
-
-//   var blockNumber = web3.eth.getBlockNumber();
-//   return blockNumber;
-// }
+import Items from './mockdata/Items';
 
 class App extends Component {
-  state = {blockNumber: 0}
-  async componentDidMount() {
-  // this.setState({blockNumber: await connect()})
-  this.connect()
+  constructor(props) {    
+    super(props);
+    this.state = {
+        items: Items
+    }
   }
-  connect = () => {
-    const web3 = new Web3(new Web3.providers.WebsocketProvider("ws://ws.nexty.io"));
-    // var a = web3.eth.getBalance("0x045bfe22453a9ca06aff4bdc5d7f5870eba121bd")
-    // console.log(a);
 
+  async componentDidMount() {
+  this.connect("0x61caAD7b6814f8D7B60bfa62dd2fC1f4d49c0872",29140000,29140050)
+  this.renderItem()
+  }
 
-    // web3.eth.subscribe("newBlockHeaders", async (error, event) => {
-    //   if (!error) {
-    //     var blockNumber = event.number;
-    //     console.log(event.number);
-    //     this.setState({
-    //       blockNumber: event.number
-    //     })
-    //     return event.number;
-    //   }
-    //   console.log(error, "loi");
-    //   return blockNumber;
-    //   });
-
-      var subscription = web3.eth.subscribe('logs', {
-        address: '0xbFC7889c6e2FF3d026f15752aaB47440d5FaA6eE',
-        topics: ['0xa8126f7572bb1fdeae5b5aa9ec126438b91f658a07873f009d041ae690f3a193']
-    }, function(error, result){
-        if (!error)
-            console.log(result);
-    })
-
-    // unsubscribes the subscription
-    subscription.unsubscribe(function(error, success){
-        if(success)
-            console.log('Successfully unsubscribed!');
+  connect = (accAddress, startBlockNumber, endBlockNumber) => {
+    const web3 = new Web3(new Web3.providers.WebsocketProvider("wss://ws.nexty.io"));
+    let {items} = this.state;
+    items.map ((item) => {
+      for (var i = startBlockNumber; i <= endBlockNumber; i++) {
+        web3.eth.getBlock(i, true, function(error, result){
+          if (!error) {
+            if (result != null && result.transactions != null) {
+              result.transactions.forEach( function(e) {
+                let id = e.input.slice(2, 10);
+                let para = '0x' + e.input.slice(11);
+                // if (accAddress === e.from || accAddress === e.to) {
+                  if (id !== "0x" && id === item.id) {
+                    // console.log(item)
+                    var str = item.function.replace(/\/\*[\s\S]*?\*\//g, '')
+                    .replace(/\/\/(.)*/g, '')          
+                    .replace(/{[\s\S]*}/, '') 
+                    .replace(/=>/g, '') 
+                    .trim(); 
+                    var start = str.indexOf("(") + 1; 
+      
+                    // End parameter names is just before last ')' 
+                    var end = str.length - 1; 
+                  
+                    var result = str.substring(start, end).split(", "); 
+                  
+                    var params = []; 
+                  
+                    result.forEach(element => { 
+                          
+                        // Removing any default value 
+                      element = element.replace(/=[\s\S]*/g, '').trim(); 
+                  
+                      if(element.length > 0) 
+                        params.push(element); 
+                    }); 
+                    let para1 = params[0];
+                    let para2 = params[1];
+                    let para3 = params[2];
+                    let para4 = params[3];
+                    let para5 = params[4];
+                    let count = params.length
+                    if (count === 0) {
+                      console.log(e.from, item.function)
+                    }
+                    if (count === 1) {
+                      var decode = web3.eth.abi.decodeParameters([para1], para);
+                      let a = item.function.indexOf('(')
+                      let b = item.function.slice(0,a+1)
+                      if(e.to === "0x0000000000000000000000000000000000023456") {
+                        console.log('nexty ' + b+decode["0"]+')');
+                      } 
+                    }
+                    if (item.count === '2') {
+                      var decode1 = web3.eth.abi.decodeParameters([para1, para2], para);
+                      console.log(item.function, decode1);
+                    }
+                    if (item.count === '3') {
+                      var decode2 = web3.eth.abi.decodeParameters([para1, para2,para3, ], para);
+                      console.log(item.function, decode2);
+                    }
+                    if (item.count === '4') {
+                      var decode3 = web3.eth.abi.decodeParameters([para1, para2, para3, para4], para);
+                      console.log(item.function, decode3);
+                    }
+                    if (item.count === '5') {
+                      var decode4 = web3.eth.abi.decodeParameters([para1, para2, para3, para4, para5], para);
+                      console.log(item.function, decode4);
+                    }
+                                     
+                  } else {
+                  }
+                // }
+              })
+            }
+          }
+        })
+      }
     });
+  }
 
-    web3._provider.on('end', (eventObj) => {
-      console.log(eventObj);
-    })
+  renderItem = () => {
+    // let {items} = this.state;
+    // return items.map((item) => {
+    //     console.log(item);
+    // });
   }
 
   render() {
---
+++ src/mockdata/Items.js
@@ -0,0 +1,172 @@
+const Items = [
+  {
+    id: "69c07d31",
+    function: "Ask()",
+  },
+  {
+    id: "6e6452cb",
+    function: "Bid()",
+  },
+  {
+    id: "f318722b",
+    function: "calcOrderID(address,bytes32)",
+  },
+  {
+    id: "43271d79",
+    function: "cancel(bool,bytes32)",
+  },
+  {
+    id: "ced4aac8",
+    function: "findAssistingID(bool,address,uint256,uint256,bytes32)",
+  },
+  {
+    id: "07c399a3",
+    function: "getOrder(bool,bytes32)",
+  },
+  {
+    id: "ee1a68c6",
+    function: "getRemainToAbsorb()",
+  },
+  {
+    id: "4ea09797",
+    function: "next(bool,bytes32)",
+  },
+  {
+    id: "be91d729",
+    function: "onBlockInitialized(uint256)",
+  },
+  {
+    id: "0d90b10a",
+    function: "prev(bool,bytes32)",
+  },
+  {
+    id: "aa1c259c",
+    function: "registerTokens(address,address)",
+  },
+  {
+    id: "8aa3f897",
+    function: "top(bool)",
+  },
+  {
+    id: "c0ee0b8a",
+    function: "tokenFallback(address,uint256,bytes)",
+  },
+  {
+    id: "dd62ed3e",
+    function: "allowance(address,address)",
+  },
+  {
+    id: "095ea7b3",
+    function: "approve(address,uint256)",
+  },
+  {
+    id: "70a08231",
+    function: "balanceOf(address)",
+  },
+  {
+    id: "313ce567",
+    function: "decimals()",
+  },
+  {
+    id: "a457c2d7",
+    function: "decreaseAllowance(address,uint256)",
+  },
+  {
+    id: "d0e30db0",
+  },
+  {
+    id: "f255606e",
+    function: "depositAndPropose(int256,uint256,uint256,uint256)",
+  },
+  {
+    id: "37a7113d",
+    function: "depositAndTrade(bytes32,uint256,uint256,bytes32)",
+  },
+  {
+    id:"b760faf9",
+    function: "depositTo(address)",
+  },
+  {
+    id: "692058c2",
+    function: "dex()",
+  },
+  {
+    id: "117f5a55",
+    function: "dexBurn(uint256)",
+  },
+  {
+    id: "bdfde911",
+    function: "dexMint(uint256)",
+  },
+  {
+    id: "39509351",
+    function: "increaseAllowance(address,uint256)",
+  },
+  {
+    id: "c4d66de8",
+    function: "initialize(address)",
+  },
+  {
+    id: "8f32d59b",
+    function: "isOwner()",
+  },
+  {
+    id: "06fdde03",
+    function: "name()",
+  },
+  {
+    id: "8da5cb5b",
+    function: "owner()",
+  },
+  {
+    id: "8137d318",
+    function: "propose(int256,uint256,uint256,uint256)",
+  },
+  {
+    id: "715018a6",
+    function: "renounceOwnership()",
+  },
+  {
+    id: "95d89b41",
+    function: "symbol()",
+  },
+  {
+    id: "b15ccada",
+    function: "totalInflated()",
+  },
+  {
+    id: "18160ddd",
+    function: "totalSupply()",
+  },
+  {
+    id: "7ca3c7c7",
+    function: "trade(bytes32,uint256,uint256,bytes32)",
+  },
+  {
+    id: "a9059cbb",
+    function: "transfer(address,uint256)",
+  },
+  {
+    id: "be45fd62",
+    function: "transfer(address,uint256,bytes)",
+  },
+  {
+    id: "23b872dd",
+    function: "transferFrom(address,address,uint256)",
+  },
+  {
+    id: "f2fde38b",
+    function: "transferOwnership(address)",
+  },
+  {
+    id: "2e1a7d4d",
+    function: "withdraw(uint256)",
+  },
+  {
+    id: "c86283c8",
+    function: "withdrawTo(uint256,address)",
+  },
+
+];
+
+export default Items;
