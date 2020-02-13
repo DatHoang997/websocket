3792c2735782aec689dd0320da1becc04fb1576b
dathoang997 <dathoang997@gmail.com>
2020-02-13 18:13:22 +0700

    input

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
         valueInput:""
     }
   }
-  connect = () => {
+  connect = async () => {
     let {valueInput} = this.state
     const web3 = new Web3(new Web3.providers.WebsocketProvider("wss://ws.nexty.io"))
 
@@ -23,7 +23,7 @@ class App extends Component {
     let {items} = this.state
     //28588311
     console.log(valueInput)
-    for (var i = valueInput-2000; i <= valueInput; i++) {
+    for (var i = valueInput; i <= valueInput; i++) {
       web3.eth.getBlock(i, true, function(error, result){
         if (!error) {
           // console.log(result)
@@ -65,60 +65,60 @@ class App extends Component {
                                 if(event.inputs.length === 5) {
                                   console.log('event ' + w + event.inputs['0'].name + ': ' + eventparam['0'] + ', ' + event.inputs['1'].name + ': ' + eventparam['1'] + ', ' + event.inputs['2'].name + ': ' + eventparam['2'] + ', ' + event.inputs['3'].name + ': ' + eventparam['3'] + ', ' + event.inputs['4'].name + ': ' + eventparam['4'] + ')', receipt.blockNumber)
                                 }
-                                var strip_comments = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg
-                                var argument_names = /([^\s,]+)/g
-                                var fnStr = item.function.replace(strip_comments, '')
-                                var parameters = fnStr.slice(fnStr.indexOf('(')+1, fnStr.indexOf(')')).match(argument_names)
-                                
-                                  parameters = []
-                                  let para1 = parameters[0]
-                                  let para2 = parameters[1]
-                                  let para3 = parameters[2]
-                                  let para4 = parameters[3]
-                                  let para5 = parameters[4]
-                                  let count = parameters.length
-                                  let a = item.function.indexOf('(')
-                                  let b = item.function.slice(0,a+1)
-                                  // console.log(e)
-                                  if (count === 0) {
-                                    console.log(e.from, item.function, e.blockNumber)
-                                    console.log('')
-                                  }
-                                  if (count === 1) {
-                                    var decode = web3.eth.abi.decodeParameters([para1], para)
-                                    // if(e.to === "0x0000000000000000000000000000000000023456") {
-                                      console.log('dex '+ b + para1 + ': ' + decode["0"]+')', e.blockNumber)
-                                      console.log('')
-                                    // } 
-                                  }
-                                  if (count === 2) {
-                                    var decode1 = web3.eth.abi.decodeParameters([para1, para2], para)
-                                    // if(e.to === "0x0000000000000000000000000000000000023456") {
-                                      console.log('dex '+ b + para1 + ': ' + decode1["0"]+', ' +  para2 + ': ' + decode1["1"]+')', e.blockNumber)
-                                      console.log('')
-                                    // } 
-                                  }
-                                  if (count === 3) {
-                                    var decode2 = web3.eth.abi.decodeParameters([para1, para2, para3], para)
-                                    // if(e.to === "0x0000000000000000000000000000000000023456") {
-                                      console.log('dex '+ b + para1 + ': ' + decode2["0"]+', '+ para2 + ': ' + decode2["1"]+', '+ para3 + ': ' + decode2["2"]+')', e.blockNumber)
-                                      console.log('')
-                                    // }                     
-                                  }
-                                  if (count === 4) {
-                                    var decode3 = web3.eth.abi.decodeParameters([para1, para2, para3, para4], para)
-                                    // if(e.to === "0x0000000000000000000000000000000000023456") {
-                                      console.log('dex '+ b + para1 + ': ' + decode3["0"]+', '+ para2 + ': ' + decode3["1"]+', '+ para3+ ': ' + decode3["2"]+', '+ para5 + ': ' + decode3["3"]+')', e.blockNumber)
-                                      console.log('')
-                                    // }                     
-                                  }
-                                  if (count === 5) {
-                                    var decode4 = web3.eth.abi.decodeParameters([para1, para2, para3, para4, para5], para)
-                                    // if(e.to === "0x0000000000000000000000000000000000023456") {
-                                      console.log('dex '+ b + para1 + ': ' + decode4["0"]+', '+ para2 + ': ' + decode4["1"]+', '+ para3+ ': ' + decode4["2"]+', '+ para4 + ': ' + decode4["3"]+', '+ para5 + ': ' + decode4["4"]+')', e.blockNumber)
-                                      console.log('')
-                                    // }                     
-                                  }
+                      var strip_comments = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg
+                      var argument_names = /([^\s,]+)/g
+                      var fnStr = item.function.replace(strip_comments, '')
+                      var parameters = fnStr.slice(fnStr.indexOf('(')+1, fnStr.indexOf(')')).match(argument_names)
+                      if(parameters === null) 
+                        parameters = []
+                        let para1 = parameters[0]
+                        let para2 = parameters[1]
+                        let para3 = parameters[2]
+                        let para4 = parameters[3]
+                        let para5 = parameters[4]
+                        let count = parameters.length
+                        let a = item.function.indexOf('(')
+                        let b = item.function.slice(0,a+1)
+                        // console.log(e)
+                        if (count === 0) {
+                          console.log(e.from, item.function, e.blockNumber)
+                          console.log('')
+                        }
+                        if (count === 1) {
+                          var decode = web3.eth.abi.decodeParameters([para1], para)
+                          // if(e.to === "0x0000000000000000000000000000000000023456") {
+                            console.log('dex '+ b + para1 + ': ' + decode["0"]+')', e.blockNumber)
+                            console.log('')
+                          // } 
+                        }
+                        if (count === 2) {
+                          var decode1 = web3.eth.abi.decodeParameters([para1, para2], para)
+                          // if(e.to === "0x0000000000000000000000000000000000023456") {
+                            console.log('dex '+ b + para1 + ': ' + decode1["0"]+', ' +  para2 + ': ' + decode1["1"]+')', e.blockNumber)
+                            console.log('')
+                          // } 
+                        }
+                        if (count === 3) {
+                          var decode2 = web3.eth.abi.decodeParameters([para1, para2, para3], para)
+                          // if(e.to === "0x0000000000000000000000000000000000023456") {
+                            console.log('dex '+ b + para1 + ': ' + decode2["0"]+', '+ para2 + ': ' + decode2["1"]+', '+ para3 + ': ' + decode2["2"]+')', e.blockNumber)
+                            console.log('')
+                          // }                     
+                        }
+                        if (count === 4) {
+                          var decode3 = web3.eth.abi.decodeParameters([para1, para2, para3, para4], para)
+                          // if(e.to === "0x0000000000000000000000000000000000023456") {
+                            console.log('dex '+ b + para1 + ': ' + decode3["0"]+', '+ para2 + ': ' + decode3["1"]+', '+ para3+ ': ' + decode3["2"]+', '+ para5 + ': ' + decode3["3"]+')', e.blockNumber)
+                            console.log('')
+                          // }                     
+                        }
+                        if (count === 5) {
+                          var decode4 = web3.eth.abi.decodeParameters([para1, para2, para3, para4, para5], para)
+                          // if(e.to === "0x0000000000000000000000000000000000023456") {
+                            console.log('dex '+ b + para1 + ': ' + decode4["0"]+', '+ para2 + ': ' + decode4["1"]+', '+ para3+ ': ' + decode4["2"]+', '+ para4 + ': ' + decode4["3"]+', '+ para5 + ': ' + decode4["4"]+')', e.blockNumber)
+                            console.log('')
+                          // }                     
+                        }
                               }
                             }
                           })
