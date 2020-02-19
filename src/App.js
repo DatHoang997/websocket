d3ad1e20311c70bbcb8537df29bbc1c2a8703261
dathoang997 <dathoang997@gmail.com>
2020-02-19 14:56:21 +0700

    full log

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, {
+  Component
+} from 'react'
 import logo from './logo.svg'
 import './App.css'
 import Web3 from 'web3'
@@ -7,88 +9,83 @@ import Events from './mockdata/Events'
 
 
 class App extends Component {
-  constructor(props) {    
+  constructor(props) {
     super(props)
     this.state = {
-        items: Items,
-        events: Events,
-        valueInput:""
+      items: Items,
+      events: Events,
+      valueInput: ""
     }
   }
   connect = async () => {
-    let {valueInput} = this.state
     const web3 = new Web3(new Web3.providers.WebsocketProvider("wss://ws.nexty.io"))
 
-    let {events} = this.state
-    let {items} = this.state
+    let {
+      valueInput,
+      events,
+      items
+    } = this.state
     //28588311
     console.log(valueInput)
     for (let i = valueInput; i <= valueInput; i++) {
-      web3.eth.getBlock(i, true, function(error, result){
+      web3.eth.getBlock(i, true, function (error, result) {
         if (!error) {
-          // console.log(result)
           if (result != null && result.transactions != null) {
-              items.forEach ((item) => {
-                result.transactions.forEach( function(e) {
-                  let id = e.input.slice(2, 10)
-                  let para = '0x' + e.input.slice(11)
-                  // if (accAddress === e.from || accAddress === e.to) {
-                    if (id !== "0x" && id === item.id) {
-                      web3.eth.getTransactionReceipt(e.hash, function(err, receipt) {
-                        if (!err && receipt.logs!== undefined && receipt.logs!== null) {
-                          // console.log(receipt)
-                          events.forEach ((event) => {
-                            for (let n = 0; n <= receipt.logs.length-1; n++) {
-                              if(event.code === receipt.logs[n].topics[0]) {
-                                // console.log(event.event, receipt.blockNumber)
-                                let ecut = event.event.indexOf('(')
-                                let ename = event.event.slice(0,ecut+1)
-                                let eventparam = web3.eth.abi.decodeLog(
-                                  event.inputs,
-                                  receipt.logs[n].data,
-                                  receipt.logs[n].topics)
-                                  let elog = 'event' + ename
-                                  for(let i = 0 ; i < event.inputs.length+1 ; i++)
-                                  {
-                                    if(i>0){
-                                      let temp = elog+event.inputs[i-1].name+": "+ eventparam[i-1]+", " ;
-                                      elog=temp
-                                  }
-                                    if(i===event.inputs.length){
-                                      elog = elog+')'
-                                      elog = elog.replace(', )',')')
-                                      console.log(elog)
-                                    }
-                                  }
-                                let strip_comments = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg
-                                let argument_names = /([^\s,]+)/g
-                                let fnStr = item.function.replace(strip_comments, '')
-                                let parameters = fnStr.slice(fnStr.indexOf('(')+1, fnStr.indexOf(')')).match(argument_names)
-                                if(parameters === null) 
-                                  parameters = []
-                                  
-                                  let fcut = item.function.indexOf('(')
-                                  let fname = item.function.slice(0,fcut+1)
-                                  let decode = web3.eth.abi.decodeParameters(parameters, para)
-                                  let flog = 'Function ' + fname
-                                  for(let i = 0 ; i < parameters.length ; i++)
-                                  {
-                                    if(i>0){
-                                      flog+=parameters[i] + ': ' + decode[i]+", " ;
-                                      
-                                  }
-                                    if(i===event.inputs.length){
-                                      flog = flog+')'
-                                      flog = flog.replace(', )',')')
-                                      console.log(flog)
-                                    }
-                                  }
+            items.forEach((item) => {
+              result.transactions.forEach(function (e) {
+                let id = e.input.slice(2, 10)
+                let para = '0x' + e.input.slice(11)
+                // if (accAddress === e.from || accAddress === e.to) {
+                if (id !== "0x" && id === item.id) {
+                  web3.eth.getTransactionReceipt(e.hash, function (err, receipt) {
+                    if (!err && receipt.logs !== undefined && receipt.logs !== null) {
+                      events.forEach((event) => {
+                        for (let n = 0; n <= receipt.logs.length - 1; n++) {
+                          if (event.code === receipt.logs[n].topics[0]) {
+                            let ecut = event.event.indexOf('(')
+                            let ename = event.event.slice(0, ecut + 1)
+                            let eventparam = web3.eth.abi.decodeLog(
+                              event.inputs,
+                              receipt.logs[n].data,
+                              receipt.logs[n].topics)
+                            let elog = 'event' + ename
+                            for (let i = 0; i < event.inputs.length + 1; i++) {
+                              if (i > 0) {
+                                let temp = elog + event.inputs[i - 1].name + ": " + eventparam[i - 1] + ", ";
+                                elog = temp
+                              }
+                              if (i === event.inputs.length) {
+                                elog = elog + ')'
+                                elog = elog.replace(', )', ')')
+                                console.log(elog)
                               }
                             }
-                          })
+                            let strip_comments = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg
+                            let argument_names = /([^\s,]+)/g
+                            let fnStr = item.function.replace(strip_comments, '')
+                            let parameters = fnStr.slice(fnStr.indexOf('(') + 1, fnStr.indexOf(')')).match(argument_names)
+                            if (parameters === null)
+                              parameters = []
+                            let fcut = item.function.indexOf('(')
+                            let fname = item.function.slice(0, fcut + 1)
+                            let decode = web3.eth.abi.decodeParameters(parameters, para)
+                            let flog = 'Function ' + fname
+                            for (let i = 0; i < parameters.length; i++) {
+                              if (i > 0) {
+                                flog += parameters[i] + ': ' + decode[i] + ", ";
+                              }
+                              if (i === event.inputs.length) {
+                                flog = flog + ')'
+                                flog = flog.replace(', )', ')')
+                                console.log(flog)
+                              }
+                            }
+                          }
                         }
                       })
                     }
+                  })
+                }
                 // }
               })
             })
@@ -98,32 +95,34 @@ class App extends Component {
     }
   }
 
-  _onchange=evt=>{
-    this.setState({valueInput:evt.target.value})
+  _onchange = evt => {
+    this.setState({
+      valueInput: evt.target.value
+    })
   }
-  _handleKeyDown=evt=>{
+  _handleKeyDown = evt => {
     if (evt.key === 'Enter') {
-      this.setState({valueInput:evt.target.value},()=>{
+      this.setState({
+        valueInput: evt.target.value
+      }, () => {
         this.connect()
       })
     }
   }
-
-
   render() {
-      return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            <label>Block Number: 28588311</label><br></br>
-            <input type="number" id="input"  onChange={this._onchange} onKeyDown={this._handleKeyDown}/><br></br>
-            <button type="submit" onClick={this.connect}>Button</button>
-          </p>
-        </header>
-      </div>
-    )
-  }
+    return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <label>Block Number: 28588311</label><br></br>
+          <input type="number" id="input"  onChange={this._onchange} onKeyDown={this._handleKeyDown}/><br></br>
+          <button type="submit" onClick={this.connect}>Button</button>
+        </p>
+      </header>
+    </div>
+  )
+}
 }
 
 export default App
